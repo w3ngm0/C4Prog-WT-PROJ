819e7ccf2b2275a60cd9288245707ee836e81157
w3ngm0 <waggmo@gmail.com>
2024-06-30 17:05:52 +0800

    fix: corrections final project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      './*.html',
+    './assets/js/**/*.js'], // Adjust paths to match your project structure
+    darkMode: 'media', // or 'media' or 'class',
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
